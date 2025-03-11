73527db97385b412ed796d6e81bab04dadf281c6
Maciej Padula <maciej.padula@wp.pl>
2025-03-12 00:03:09 +0100

    #6 comments

--- src/ComponentGallery/ComponentGallery.Server/Program.cs
+++ src/ComponentGallery/ComponentGallery.Server/Program.cs
@@ -3,6 +3,7 @@ using ComponentGallery.Server.Features.Components;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 builder.Services.AddComponentsServices();
 
 var app = builder.Build();
@@ -10,7 +11,8 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.MapStaticAssets();
 
-app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
