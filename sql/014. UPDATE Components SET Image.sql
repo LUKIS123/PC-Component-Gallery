581489d16ad6791d5240f464448c8276fda52fa8
Lukasz Gawron <51411768+LUKIS123@users.noreply.github.com>
2025-04-27 11:41:28 +0200

    components update set imageUrls

---
+++ sql/014. UPDATE Components SET Image.sql
@@ -0,0 +1,31 @@
+UPDATE [Gallery].[Components]
+SET ImageUrl = 'https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2020/10/pr_2020_10_9_13_56_59_540_00.jpg'
+WHERE Id = 1;
+
+UPDATE [Gallery].[Components]
+SET ImageUrl = 'https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2018/8/pr_2018_8_20_9_22_9_710_02.jpg'
+WHERE Id = 2;
+
+UPDATE [Gallery].[Components]
+SET ImageUrl = 'https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2020/12/pr_2020_12_8_14_0_47_951_09.jpg'
+WHERE Id = 3;
+
+UPDATE [Gallery].[Components]
+SET ImageUrl = 'https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2019/8/pr_2019_8_14_12_6_52_541_00.jpg'
+WHERE Id = 4;
+
+UPDATE [Gallery].[Components]
+SET ImageUrl = 'https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2018/7/pr_2018_7_24_18_15_43_780_00.jpg'
+WHERE Id = 5;
+
+UPDATE [Gallery].[Components]
+SET ImageUrl = 'https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2018/8/pr_2018_8_6_9_10_53_188_02.jpg'
+WHERE Id = 6;
+
+UPDATE [Gallery].[Components]
+SET ImageUrl = 'https://dlcdnwebimgs.asus.com/gain/A90D5381-1C0D-4A98-AE08-AC630F263D5C/w717/h525'
+WHERE Id = 7;
+
+UPDATE [Gallery].[Components]
+SET ImageUrl = 'https://dlcdnwebimgs.asus.com/gain/5D027C28-F87D-45C2-B7F1-110AFB343531/w717/h525'
+WHERE Id = 8;
